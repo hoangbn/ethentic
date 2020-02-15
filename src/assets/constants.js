fe8e47a571dcd96df879be374c0c451e4c2f9ed1
Hoang Nguyen <minh.hoang1297@gmail.com>
2020-02-15 10:37:24 -0500

    Update app structure

---
+++ src/assets/constants.js
@@ -0,0 +1,7 @@
+import { AppConfig } from 'blockstack'
+
+export const appConfig = new AppConfig(['store_write', 'publish_data']);
+
+export const USER_INFO_FILE = 'userInfo.json';
+
+export const EXPLORER_URL = 'https://explorer.blockstack.org';
