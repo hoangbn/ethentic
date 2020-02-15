f14e956f6275c315002119df982e00efedbbb28b
Hoang Nguyen <minh.hoang1297@gmail.com>
2020-02-15 11:24:41 -0500

    Refactor

--- src/assets/constants.js
+++ src/assets/constants.js
@@ -3,5 +3,3 @@ import { AppConfig } from 'blockstack'
 export const appConfig = new AppConfig(['store_write', 'publish_data']);
 
 export const USER_INFO_FILE = 'userInfo.json';
-
-export const EXPLORER_URL = 'https://explorer.blockstack.org';
--- src/assets/utils.js
+++ src/assets/utils.js
@@ -7,21 +7,21 @@ export function jsonCopy(object) {
 
 // All three functions take in a state and return a list object of tasks
 export function remove(index, state) {
-  const tasks = jsonCopy(state.tasks)
-  tasks.splice(index, 1) // remove subject at index
+  const tasks = jsonCopy(state.tasks);
+  tasks.splice(index, 1); // remove subject at index
   return tasks
 }
 
 export function add(state) {
-  const task = state.value
-  const tasks = jsonCopy(state.tasks)
-  tasks.push([task, false])
+  const task = state.value;
+  const tasks = jsonCopy(state.tasks);
+  tasks.push([task, false]);
   return tasks
 }
 
 export function check(index, state) {
-  const tasks = jsonCopy(state.tasks)
-  tasks[index][1] = !tasks[index][1]
+  const tasks = jsonCopy(state.tasks);
+  tasks[index][1] = !tasks[index][1];
   return tasks
 }
 
