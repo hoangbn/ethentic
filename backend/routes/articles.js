3b232747b4d3b1e30a06f6f0dd6a7a7d040fa8a6
Hoang Nguyen <minh.hoang1297@gmail.com>
2020-02-15 20:15:08 -0500

    Add google truth check

--- backend/routes/articles.js
+++ backend/routes/articles.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
 let Article = require('../models/articleModel');
+
+const GOOGLE_API_URL = "https://factchecktools.googleapis.com/v1alpha1/claims:search";
 
 router.get('/', (req, res) => {
     Article.find()
@@ -8,12 +11,22 @@ router.get('/', (req, res) => {
         .catch(err => res.status(500).send(err));
 });
 
-router.post('/', (req, res) => {
-    const { title, content, isTrue, source } = req.body;
-    const newArticle = new Article({title, content, isTrue, source });
-    newArticle.save()
-        .then(article => res.send(article))
-        .catch(err => res.status(500).send(err));
+router.post('/', async (req, res) => {
+    const { title, content, source } = req.body;
+    try {
+        const truthData = (await axios.get(GOOGLE_API_URL, {
+            params: {
+                query: content,
+                key: process.env.GOOGLE_API_KEY
+            }
+        })).data;
+        if (!truthData.claims) return res.status(404).send("Couldn't evaluate the truth of the article");
+        const isTrue  = truthData.claims[0].claimReview[0].textualRating === "True";
+        const newArticle = new Article({title, content, source, isTrue });
+        res.send(await newArticle.save());
+    } catch (err) {
+        res.status(500).send(err);
+    }
 });
 
 router.get('/:id', (req, res) => {
@@ -25,11 +38,11 @@ router.get('/:id', (req, res) => {
 router.put('/:id/addUserReview', async (req, res) => {
     try {
         const article = await Article.findOne({ _id: req.params.id });
-        if (!article) return res.status(404).send("article not found");
+        if (!article) return res.status(404).send("Article not found");
         article.userReviewCount++;
         res.send(await article.save());
     } catch(err) {
-        res.status(500).send(err)
+        res.status(500).send(err);
     }
 });
 
@@ -37,6 +50,13 @@ router.delete('/:id', (req, res) => {
     Article.deleteOne({ _id: req.params.id }, err => {
         if (err) return res.status(500).send(err);
         res.end();
+    })
+});
+
+router.delete('/', (req, res) => {
+    Article.deleteMany({}, err => {
+        if (err) return res.status(500).send(err);
+        res.send("Deleted all documents");
     })
 });
 
