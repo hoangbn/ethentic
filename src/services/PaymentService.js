f55aeb2ccc373c1771602bf351726e2e1f7dd970
Hoang Nguyen <minh.hoang1297@gmail.com>
2020-02-15 22:37:43 -0500

    Payment fix

--- src/services/PaymentService.js
+++ src/services/PaymentService.js
@@ -1,8 +1,9 @@
 import web3 from '../utils/web3';
 
-const ETH_ADDRESS = '0x8fE9cCFEA956C69e4bd515a946205e1f839cCaC7';
-const value = '0xde0b6b3a7640000'; // an ether has 18 decimals, here in hex.
+const ETH_ADDRESS = '0x41B398EA4497c8484bF243D62A761046aF25B5Df';
+const WEI_TO_ETHER = 10**18;
 //const desiredNetwork = '4'; // '1' is the Ethereum main network ID.
+const value = WEI_TO_ETHER * 44;
 
 export default class PaymentService {
   static isEthereumCompatible() {
@@ -17,7 +18,6 @@ export default class PaymentService {
   static receivePayment() {
     if (!this.isEthereumCompatible()) return;
     let ethereum = window.ethereum;
-    web3.eth.getBalance("0x8fE9cCFEA956C69e4bd515a946205e1f839cCaC7").then(r => {console.log(r)});
     // ask user to sign in and reveal their accounts with MetaMask
     ethereum.enable()
         // Remember to handle the case they reject the request:
@@ -37,7 +37,7 @@ export default class PaymentService {
           //}
           // suggest transactions and signatures:
           const account = accounts[0];
-          this.sendPayment(ethereum, account, ETH_ADDRESS, function (err, transaction) {
+          this.sendPayment(account, ETH_ADDRESS, function (err, transaction) {
             if (err) return alert(`An error has occurred!`);
             alert('Purchase Successful!')
           })
@@ -47,22 +47,16 @@ export default class PaymentService {
   static sendPayment(sendingAccount, receivingAccount, callback) {
     if (!this.isEthereumCompatible()) return;
     let ethereum = window.ethereum;
-    console.log(ethereum);
+
     // We're going to use the lowest-level API here, with simpler example links below
     const method = 'eth_sendTransaction';
     const parameters = {
-      from: "0x6bdb46dccf4849f21f5ca2181c8f206d7fb715e4",
+      from: sendingAccount,
       to: receivingAccount,
       value: value,
     };
 
     // Now putting it all together into an RPC request:
-    const payload = {
-      method: method,
-      params: parameters,
-      from: sendingAccount,
-    };
-    console.log(parameters);
 
     web3.eth.sendTransaction(parameters, (err, response) => {
     //
@@ -79,7 +73,6 @@ export default class PaymentService {
         return alert(`We can't take your money without your permission.`)
       }
 
-      console.log(response);
       if (response.result) {
         // If there is a response.result, the call was successful.
         // In the case of this method, it is a transaction hash.
